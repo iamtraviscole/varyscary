6e015eb993aa70fd6af69f7a4d64390f12e2ab3e
Travis Cole <iamtraviscole@gmail.com>
2018-05-01 19:41:29 -0400

    cleaned up Splash css, updated css color names

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -7,10 +7,10 @@ const Splash = (props) => {
   return (
     <div className='Splash'>
       <div className='Splash__ctr'>
-        <h1>Make some monsters</h1>
-        <Link to='/signup' className='Splash__signup-btn'>Sign Up</Link>
+        <h1 className='Splash__h1'>Make some monsters</h1>
+        <Link to='/signup' className='Splash__btn'>Sign Up</Link>
         <br />
-        <Link to='/login' className='Splash__login-btn'>Log In</Link>
+        <Link to='/login' className='Splash__btn Splash__btn--login'>Log In</Link>
       </div>
     </div>
   )
