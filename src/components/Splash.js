3df120c609587c334f01be971191a825a02e9911
Travis Cole <iamtraviscole@gmail.com>
2018-05-01 01:42:46 -0400

    changed some Splash component styling and added css variables for colors

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -9,8 +9,8 @@ const Splash = (props) => {
       <div className='Splash__ctr'>
         <h1>Make some monsters</h1>
         <Link to='/signup' className='Splash__signup-btn'>Sign Up</Link>
-        <p>Already have an account?</p>
-        <Link to='/login' className='Splash__login'>Log In</Link>
+        <br />
+        <Link to='/login' className='Splash__login-btn'>Log In</Link>
       </div>
     </div>
   )
