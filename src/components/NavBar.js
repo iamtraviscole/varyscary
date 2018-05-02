2cf9db2d6b5f768834ab25705015c44e6177314d
Travis Cole <iamtraviscole@gmail.com>
2018-05-02 01:42:09 -0400

    added google material icons and updated NavBar css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,19 +5,19 @@ import '../styles/NavBar.css'
 
 const NavBar = (props) => {
   return (
-    <div className='NavBar'>
-      <nav>
-        <ul className='NavBar__ul'>
-          <li className='NavBar__li NavBar__li--logo'>Monster Maker</li>
-          <div className='NavBar__right'>
-            <li className='NavBar__li'><NavLink to={'/' + props.username}>Home</NavLink></li>
-            <li className='NavBar__li'><NavLink to='/monsters/new'>Make</NavLink></li>
-            <li className='NavBar__li'><NavLink to='/monsters'>Explore</NavLink></li>
-            <li className='NavBar__li NavBar__li--right'><NavLink to='/'>Log Out</NavLink></li>
-          </div>
-        </ul>
-      </nav>
-    </div>
+    <nav className='NavBar'>
+      <ul className='NavBar__ul'>
+        <li className='NavBar__li NavBar__li--logo'>Monster Maker</li>
+        <div className='NavBar__right'>
+          <li className='NavBar__li'><NavLink to={'/' + props.username}>Home</NavLink></li>
+          <li className='NavBar__li'><NavLink to='/monsters/new'>Make</NavLink></li>
+          <li className='NavBar__li'><NavLink to='/monsters'>Explore</NavLink></li>
+          <li className='NavBar__li'><NavLink to='/'>
+            <i className='material-icons'>account_circle arrow_drop_down</i>
+          </NavLink></li>
+        </div>
+      </ul>
+    </nav>
   )
 }
 
