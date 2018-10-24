7148f9a06f22e6b1dae5eff24e460eed1fa85cba
Travis Cole <iamtraviscole@gmail.com>
2018-10-24 05:07:13 -0400

    updated colors and styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,29 +22,29 @@ class NavBar extends Component {
 
   render () {
     let navItems = (
-      <div className='NavBar__right'>
-        <li className='NavBar__li'><NavLink to='/home'>Home</NavLink></li>
-        <li className='NavBar__li'><NavLink to='/monsters/new'>Make</NavLink></li>
-        <li className='NavBar__li'><NavLink to='/monsters'>Explore</NavLink></li>
-        <li className='NavBar__li'>
+      <Fragment>
+        <div className='NavBar__link-ctr'>
+          <li className='NavBar__link-li'><NavLink to='/home' className='NavBar__link'>Home</NavLink></li>
+          <li className='NavBar__link-li'><NavLink to='/monsters/new' className='NavBar__link'>Make</NavLink></li>
+          <li className='NavBar__link-li'><NavLink to='/monsters' className='NavBar__link'>Explore</NavLink></li>
+        </div>
+        <li className='NavBar__user-panel-li'>
           <button onClick={this.handleUserPanelClick} className='NavBar__user-panel-btn'>
             {!this.state.showUserPanel
-              ? <i className='material-icons'>account_circle</i>
+              ? <i className='material-icons'>person</i>
               : <i className='material-icons'>close</i> }
           </button>
         </li>
-      </div>
+      </Fragment>
     )
 
     if (this.props.userOnMobile) {
       navItems = (
-        <div className='NavBar__right'>
-          <li className='NavBar__li'>
-            <button onClick={this.handleSlideoutClick} className='NavBar__user-panel-btn'>
-              <i className='material-icons'>menu</i>
-            </button>
-          </li>
-        </div>
+        <li className='NavBar__hamburger-menu-li'>
+          <button onClick={this.handleSlideoutClick} className='NavBar__hamburger-menu'>
+            <i className='material-icons'>menu</i>
+          </button>
+        </li>
       )
     }
 
@@ -66,8 +66,8 @@ class NavBar extends Component {
               handleUserPanelClick={this.handleUserPanelClick}
               username={this.props.username} />}
         <nav className='NavBar'>
+          <div className='NavBar__logo'>Logo Here</div>
           <ul className='NavBar__ul'>
-            <li className='NavBar__li NavBar__li--logo'>Monster Maker</li>
             {navItems}
           </ul>
         </nav>
