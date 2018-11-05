5ca168384b9b89fedc6a01f57b9835ec873869df
Travis Cole <iamtraviscole@gmail.com>
2018-11-04 23:08:33 -0500

    added links and styled Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+import popupMonster1 from '../images/popup-monster-1.svg'
+import popupMonster2 from '../images/popup-monster-2.svg'
+import popupMonster3 from '../images/popup-monster-3.svg'
+import popupMonster4 from '../images/popup-monster-4.svg'
 
 import '../styles/Home.css'
 
-const Home = (props) => {
-  return (
-    <div className='Home'>
-      <p>Home</p>
-    </div>
-  )
+class Home extends Component {
+  render() {
+    return (
+      <div className='Home'>
+        <div className='Home__btns-ctr'>
+          <div className='Home__btn-ctr'>
+            <div className='Home__popup-monster Home__popup-monster-1'>
+              <img src={popupMonster1} alt='monster'></img>
+            </div>
+            <Link to='/monsters/new' className='Home__big-btn Home__make-btn'>
+              <div className='Home__text'>
+                Make
+              </div>
+            </Link>
+          </div>
+          <div className='Home__btn-ctr'>
+            <div className='Home__popup-monster Home__popup-monster-2'>
+              <img src={popupMonster2} alt='monster'></img>
+            </div>
+            <Link to='/monsters' className='Home__big-btn Home__explore-btn'>
+              <div className='Home__text'>
+                Explore
+              </div>
+            </Link>
+          </div>
+          <div className='Home__btn-ctr'>
+            <div className='Home__popup-monster Home__popup-monster-3'>
+              <img src={popupMonster3} alt='monster'></img>
+            </div>
+            <Link to={`/${this.props.username}`} className='Home__big-btn Home__your-monsters-btn'>
+              <div className='Home__text'>
+                Your Monsters
+              </div>
+            </Link>
+          </div>
+          <div className='Home__btn-ctr'>
+            <div className='Home__popup-monster Home__popup-monster-4'>
+              <img src={popupMonster4} alt='monster'></img>
+            </div>
+            <Link to='/home' className='Home__big-btn Home__favorites-btn'>
+              <div className='Home__text'>
+                Your Favorites
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    username: state.username
+  }
+}
+
+export default connect(mapStateToProps)(Home)
