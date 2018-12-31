9ce2eb3ed5fc85cac19d8aa0e1c144fa94058783
Travis Cole <iamtraviscole@gmail.com>
2018-12-31 00:19:00 -0500

    cleaned up NoAuthNavBar

--- src/components/NoAuthNavBar.js
+++ src/components/NoAuthNavBar.js
@@ -6,20 +6,14 @@ import '../styles/NoAuthNavBar.css'
 const NoAuthNavBar = (props) => {
   return (
     <nav className='NoAuthNavBar'>
-      <ul className='NoAuthNavBar__ul'>
-        <li className='NoAuthNavBar__li NoAuthNavBar__li--logo'>Monster Maker</li>
+      <div className='NoAuthNavBar__logo'>Logo Here</div>
         <div className='NoAuthNavBar__right'>
-          <li className='NoAuthNavBar__li'>
             <Link className='NoAuthNavBar__btn NoAuthNavBar__btn--login'
               to='/login'>
                 Log In
             </Link>
-          </li>
-          <li className='NoAuthNavBar__li'>
             <Link className='NoAuthNavBar__btn' to='/signup'>Sign Up</Link>
-          </li>
         </div>
-      </ul>
     </nav>
   )
 }
